0d2b6e91961b305ea42171c38aa1815d2839310d
Antonio Natali <agoognat@gmail.com>
2021-01-08 17:28:29 +0100

    upload artiftacs

--- app/src/test/kotlin/cidemo/AppTest.kt
+++ app/src/test/kotlin/cidemo/AppTest.kt
@@ -9,7 +9,7 @@ import kotlin.test.assertNotNull
 class AppTest {
     @Test fun testAppHasAGreeting() {
         val classUnderTest = App()
-        //assertNotNull(classUnderTest.greeting, "app should have a greeting")
-        assert(2==0)
+        assertNotNull(classUnderTest.greeting, "app should have a greeting")
+        //assert(2==0)   //to check what happens with a failure
     }
 }
