e0782bb3c1024a1ccd0012d6191ccd7afa49847a
Antonio Natali <antonio.natali@unibo.it>
2021-01-09 10:22:24 +0100

    sysRule.pl and demo.pl in the main dir

--- app/src/main/kotlin/cidemo/UtilsInKotlin.kt
+++ app/src/main/kotlin/cidemo/UtilsInKotlin.kt
@@ -8,12 +8,15 @@ object UtilsInKotlin {
 
     fun loadRules() {
         try {
-            val userDirPath = System.getProperty("user.dir").replace("\\","/").replace("/app","")
+            val userDirPath = System.getProperty("user.dir").replace("\\","/")
             //userDirPath is C:\Didattica2018Work\cidemo\app when run the test (?)
+            var kbDir = "."
+            if( userDirPath.contains("/app"))  kbDir = ".."
             println("UtilsInKotlin | loadRules userDirPath= $userDirPath")
-            val sol1 = engine.solve("consult('$userDirPath/unibolibs/sysRules.pl').")
+            //val sol1 = engine.solve("consult('$userDirPath/unibolibs/sysRules.pl').")
+            val sol1 = engine.solve("consult('$kbDir/sysRules.pl').")
             println("UtilsInKotlin | loadRules load sysRules.pl= $sol1")
-            val sol2 = engine.solve("consult('demo0.pl').") //tuprolog works on unibolibs
+            val sol2 = engine.solve("consult('./demo0.pl').") //tuprolog consult changes the dir
             println("UtilsInKotlin | loadRules load demo0.pl= $sol2")
         } catch (e: Exception) {
             println("UtilsInKotlin | loadRules ERROR " + e.message)
